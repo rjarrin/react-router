ab578784cda60d1982e789172a816bb5f9cacc5b
rjarrin <jarrin.ricardo@gmail.com>
2024-06-18 14:29:33 -0400

    Completed react router lesson

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import App from './App.jsx'
+// import Profile from './Profile.jsx'
+// import Spinach from './Spinach.jsx'
+// import Popeye from './Popeye.jsx'
+// import DefaultProfile from './DefaultProfile.jsx'
+// import ErrorPage from './ErrorPage.jsx'
+import routes from './routes'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />
+//   },
+//   {
+//     path: "profile/:name",
+//     element: <Profile />,
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
+
